2068643219527674d93ff95759c39f6e1e0b7035
manoy96 <mfewcar@gmail.com>
2020-02-18 18:22:38 -0700

    basic fullpage.js working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 new fullpage('#fullpage', {
 	//options here
 	autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
 });
 
 //methods
