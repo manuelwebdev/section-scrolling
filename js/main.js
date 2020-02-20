00c5bbf12096ab14d9808073f1e5cc45ce19d268
manoy96 <mfewcar@gmail.com>
2020-02-20 00:04:50 -0700

    setAllowScrolling  set to true to allow scrolling with mouse/trackpad

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-new fullpage('#fullpage', {
-	//options here
+var myFullpage = new fullpage('#fullpage', {
+    //options here
+    css3: true,
 	autoScrolling:true,
     scrollHorizontally: true,
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+    sectionsColor: ['#666666', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+    paddingTop: '1em',
+    keyboardScrolling: true,
 });
 
 //methods
-fullpage_api.setAllowScrolling(false);
+fullpage_api.setAllowScrolling(true);
