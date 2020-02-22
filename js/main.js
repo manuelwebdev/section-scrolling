cf250beb9f5f303c65c3636a3e3325c3610092b4
manoy96 <mfewcar@gmail.com>
2020-02-22 14:43:48 -0700

    added continuous scroll

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ var myFullpage = new fullpage('#fullpage', {
 	autoScrolling:true,
     scrollHorizontally: true,
     sectionsColor: ['rgba(#000000, 0)', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    // paddingTop: '1em',
     keyboardScrolling: true,
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['Section 1', 'Section 2','Section 3','Section 4'],
     fitToSection: true,
+    // loopBottom: true,
+    continuousVertical: true,
 });
 
 //methods
